f1dc7eb5b5275f4db04aee3a83caeee6bab4bcb1
Jan <jan@rannik.eu>
2024-02-07 22:39:55 +0200

    Add IY files

---
+++ app.js
@@ -0,0 +1,21 @@
+const displayResult = document.querySelector("#result");
+const btnGetAPrediction = document.querySelector("button");
+
+const options = [
+    'You will find your love sooner than you think.', 
+    'You will win a lottery.',
+    'You will lose your phone. Check your right pocket.',
+    'You will fall asleep on a couch.',
+    'You will be taking a ring to Mordor.',
+    'You will get your letter from Harvard.',
+    'Elon Musk will buy TikTok.',
+    'Your find a very fortunate opportunity. Be alert, find it and take it.'
+];
+
+btnGetAPrediction.addEventListener('click', () => {
+    const randomFortuneIndex = Math.floor(Math.random () *options.length);    
+    const prediction = options[randomFortuneIndex];
+
+    resultMessage =
+    displayResult.innerHTML = prediction;
+});
